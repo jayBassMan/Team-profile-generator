7f6b806d9f77ba9859d89a76cb2d00f32e092b37
Jonathan Johnson <jonathandila682@gmail.com>
2022-04-03 07:19:39 -0500

    refactor js code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const inquirer = require("inquirer");
+const inquirer = require('inquirer')
 const fs = require("fs");
 const { profile } = require("console");
 const Engineer = require("./lib/Engineer");
